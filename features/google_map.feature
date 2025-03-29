3413cc89781c52c31a7290daa5d015138ed0e30c
a00593708 <gundalatejeswini@gmail.com>
2025-03-29 23:48:39 +0530

    Did some changes in step 3

--- features/google_map.feature
+++ features/google_map.feature
@@ -1,7 +1,15 @@
-Feature: Search for nearest Restauranbt in Google Maps
-  Scenario: Get Details of the Nearest Restaurant
+#Feature: Search for nearest Restauranbt in Google Maps
+#  Scenario: Get Details of the Nearest Restaurant
+#    Given The Google Map Application is open
+#    When I search for nearest Restaurant
+#    Then I able to select the first five Restaurants in the list
+#    Then I see the name,Location,Contact Number,Rating of the Restaurant
+#    Then I see the lattitute and longuitude of the Restaurant
+
+
+
+ Feature: Searching for nearest restaurants and getting details
+  Scenario: Search for nearest restaurants and get details
     Given The Google Map Application is open
-    When I search for nearest Restaurant
-    Then I able to select the first five Restaurants in the list
-    Then I see the name,Location,Contact Number,Rating of the Restaurant
-    Then I see the lattitute and longuitude of the Restaurant
+    When I search for nearest restaurants
+    Then I should able to get the details of each Restaurant
--- features/steps/google_map_steps.py
+++ features/steps/google_map_steps.py
@@ -9,13 +9,42 @@ page = tab.new_page()
 
 def for_timeout(a):
     page.wait_for_timeout(a)
+def name():
+    name=page.wait_for_selector("(//div[@role='tablist']//preceding::h1)[3]").text_content()
+    return name
+def rating():
+    rating=page.wait_for_selector("((//div[@role='tablist']//preceding::h1)[3]//following::span)[4]").text_content()
+    return rating
+def location():
+    location=page.wait_for_selector("((//span[@class='google-symbols PHazN'])[1]//following::div)[2]").text_content()
+    return location
+def number():
+    number= page.wait_for_selector("(//span[@class='google-symbols NhBTye PHazN']//following::div)[2]").text_content()
+    return number
+def lattitude():
+    match = re.search(r"@(-?\d+\.\d+),(-?\d+\.\d+)",
+                      "https://www.google.com/maps/place/Maryada+Ramanna+Multi+Cusine+Restaurant/@17.4581993,78.3670023,17z/data=!3m1!5s0x3bcb93cfc6f689b7:0x73d2f8bd060bc177!4m10!1m2!2m1!1snearest+restaurants!3m6!1s0x3bcb93cbb1ff856d:0xf03169e4a2bff485!8m2!3d17.4581993!4d78.3715084!15sChNuZWFyZXN0IHJlc3RhdXJhbnRzIgOQAQFaFSITbmVhcmVzdCByZXN0YXVyYW50c5IBF3NvdXRoX2luZGlhbl9yZXN0YXVyYW504AEA!16s%2Fg%2F11n6rglg76?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D")
+    if match:
+        latitude = match.group(1)
+        return latitude
+    else:
+        return "Could not find latitude  in the URL."
+def longuitude():
+    match = re.search(r"@(-?\d+\.\d+),(-?\d+\.\d+)",
+                      "https://www.google.com/maps/place/Maryada+Ramanna+Multi+Cusine+Restaurant/@17.4581993,78.3670023,17z/data=!3m1!5s0x3bcb93cfc6f689b7:0x73d2f8bd060bc177!4m10!1m2!2m1!1snearest+restaurants!3m6!1s0x3bcb93cbb1ff856d:0xf03169e4a2bff485!8m2!3d17.4581993!4d78.3715084!15sChNuZWFyZXN0IHJlc3RhdXJhbnRzIgOQAQFaFSITbmVhcmVzdCByZXN0YXVyYW50c5IBF3NvdXRoX2luZGlhbl9yZXN0YXVyYW504AEA!16s%2Fg%2F11n6rglg76?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D")
+    if match:
+        longitude = match.group(2)
+        return longitude
+    else:
+        return "Could not find longitude in the URL."
+
 
 @Given('The Google Map Application is open')
 def open_google_maps(context):
     page.goto("https://www.google.com/maps")
     for_timeout(3000)
 
-@When('I search for nearest Restaurant')
+@When('I search for nearest Restaurants')
 def search_nearest_restaurant(context):
     search_bar=page.wait_for_selector("//input[@role='combobox']")
     search_bar.type("nearest Restaurants")
@@ -24,39 +53,31 @@ def search_nearest_restaurant(context):
     for_timeout(3000)
 
 
-@Then('I able to select the first five Restaurants in the list')
+@Then('I should able to get the details of each Restaurant')
 def open_first_one(context):
     for i in range(5):
-        elements=page.wait_for_selector(f"")
-        page.click("((//span[text()='Share'])[1]//following::div)[1]")
-        for_timeout(3000)
-
-@Then('I see the name,Location,Contact Number,Rating of the Restaurant')
-def get_name(context):
-    name=page.wait_for_selector("(//div[@role='tablist']//preceding::h1)[3]")
-    print(name.text_content())
-    page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
-
-    rating = page.wait_for_selector("((//div[@role='tablist']//preceding::h1)[3]//following::span)[4]")
-    print(rating.text_content())
-    page.wait_for_timeout(3000)
-    page.evaluate('window.scrollBy(0, 500)')
-
-    address = page.wait_for_selector("((//span[@class='google-symbols PHazN'])[1]//following::div)[2]")
-    print(address.text_content())
-    page.evaluate('window.scrollBy(0, 500)')
-
-    number = page.wait_for_selector("(//span[@class='google-symbols NhBTye PHazN']//following::div)[2]")
-    print(number.text_content())
-    page.evaluate('window.scrollBy(0, 500)')
-
-
-@Then('I see the lattitute and longuitude of the Restaurant')
-def get_lattitude_and_longuitude(context):
-    match = re.search(r"@(-?\d+\.\d+),(-?\d+\.\d+)", "https://www.google.com/maps/place/Maryada+Ramanna+Multi+Cusine+Restaurant/@17.4581993,78.3670023,17z/data=!3m1!5s0x3bcb93cfc6f689b7:0x73d2f8bd060bc177!4m10!1m2!2m1!1snearest+restaurants!3m6!1s0x3bcb93cbb1ff856d:0xf03169e4a2bff485!8m2!3d17.4581993!4d78.3715084!15sChNuZWFyZXN0IHJlc3RhdXJhbnRzIgOQAQFaFSITbmVhcmVzdCByZXN0YXVyYW50c5IBF3NvdXRoX2luZGlhbl9yZXN0YXVyYW504AEA!16s%2Fg%2F11n6rglg76?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D")
-    if match:
-        latitude = match.group(1)
-        longitude = match.group(2)
-        print(f"Latitude: {latitude}, Longitude: {longitude}")
-    else:
-        print("Could not find latitude and longitude in the URL.")
+        element = 1
+        # reserve=1
+        for i in range(21):
+            # if page.query_selector(f"(//a[@target='_self']){[reserve]}").is_visible:
+            page.wait_for_selector(f"((//span[text()='Share'])[1]//following::a){[element]}").click()
+            if page.query_selector("//div[text() = 'Reserve a table']").is_visible:
+                page.wait_for_timeout(3000)
+                # reserve+=1
+                element += 2
+                print(name())
+                print(rating())
+                print(location())
+                print(number())
+                print(lattitude())
+                print(longuitude())
+            else:
+                # page.wait_for_selector(f"((//span[text()='Share'])[1]//following::a){[element]}").click()
+                # page.wait_for_timeout(3000)
+                element += 1
+                print(name())
+                print(rating())
+                print(location())
+                print(number())
+                print(lattitude())
+                print(longuitude())
