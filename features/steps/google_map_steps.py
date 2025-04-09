97c63245d77fec8e2131320ccaaaee50ce3294d4
Sudhanshu <sudanshu.pandey@external.atlascopco.com>
2025-04-09 14:49:37 +0530

    made some changes

--- features/steps/google_map_steps.py
+++ features/steps/google_map_steps.py
@@ -4,16 +4,19 @@ import re
 import pandas as pd
 from playwright.sync_api import sync_playwright
 
-
 playwright_start = sync_playwright().start()
 browser = playwright_start.chromium.launch(headless=False)
 tab = browser.new_context(viewport={ 'width': 1890, 'height': 920 })
 page = tab.new_page()
 
+application_url="https://www.google.com/maps"
+input_locator="//input[@role='combobox']"
+button_locator="//button[@id='searchbox-searchbutton']"
+
 Locators={"Name":"(//div[@role='tablist']//preceding::h1)[3]",
           "Rating":"((//div[@role='tablist']//preceding::h1)[3]//following::span)[4]",
           "Location":"((//span[@class='google-symbols PHazN'])[1]//following::div)[2]",
-          "Number":"(//span[@class='google-symbols NhBTye PHazN']//following::div)[2]"
+          "Number":"((//span[text()=''])[1]//following::div)[2]"
 }
 
 def for_timeout(a):
@@ -22,8 +25,8 @@ def for_timeout(a):
 def get_details(selectors):
     details = {}
     for key, selector in selectors.items():
-        element = page.locator(selector)
-        details[key] = element.text_content()
+        element = page.locator(selector).text_content().replace(" ","")
+        details[key]=element
     return details
 
 
@@ -34,23 +37,18 @@ def get_coordinates():
         longitude = match.group(2)
         return latitude, longitude
     else:
-        return "Could not find latitude and longitude in the URL."
-
+        return "Could not find latitude and longitude"
 
 @Given('User open Google map Application')
 def open_google_maps(context):
-    page.goto("https://www.google.com/maps")
+    page.goto('https://www.google.com/maps')
     for_timeout(3000)
-
 
 @When('User search for nearest Restaurants')
 def search_nearest_restaurant(context):
-    search_bar = page.locator("//input[@role='combobox']")
-    search_bar.type("nearest Restaurants")
-    search_button = page.locator("//button[@id='searchbox-searchbutton']")
-    search_button.click()
+    page.locator(input_locator).type("nearest Restaurants")
+    page.locator(button_locator).click()
     for_timeout(3000)
-
 
 @Then('User should able to open and get the details of each Restaurant')
 def open_first_one(context):
@@ -58,16 +56,14 @@ def open_first_one(context):
     element = 1
     visited_urls=set()
 
-    while len(result_list) < 30:
+    while len(result_list) < 20:
         restaurant_container = page.locator(f"(//div[@class='Nv2PK THOPZb CpccDe '])[{element}]")
 
         if restaurant_container.is_visible():
             restaurant_container.click()
 
             current_url=page.url
-
             if current_url in visited_urls:
-                page.keyboard.press("PageDown")
                 continue
             visited_urls.add(current_url)
 
@@ -77,14 +73,12 @@ def open_first_one(context):
                 details["Longuitude"] = get_coordinates()[1]
 
                 result_list.append(details)
-                for_timeout(2000)
                 element += 1
             else:
                 page.keyboard.press("PageDown")
         else:
             page.keyboard.press("PageDown")
-            for_timeout(5000)
-    print(result_list)
+
     dataframe = pd.DataFrame(result_list)
     dataframe.to_csv("restaurants_details.csv", index=False)
 
